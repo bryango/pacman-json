2ade0883766600ebbc6d21c3ab57894185f77854
Bryan Lai <bryanlais@gmail.com>
2023-07-31 13:57:09 +0800

    Reproduce pacman -Qe as a proof of concept

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,12 @@ where
         .expect("failed to call pacman-conf")
         .stdout;
 
-    return String::from_utf8_lossy(&cmd_out).to_string();
+    let out_string = String::from_utf8_lossy(&cmd_out).to_string();
+
+    match out_string.strip_suffix('\n') {
+        Some(x) => x.to_string(),
+        None => out_string
+    }
 }
 
 /// Parses and updates the SigLevel from the cli `pacman-conf`
@@ -26,8 +31,8 @@ fn update_siglevel(siglevel: &str, original: SigLevel) -> SigLevel {
     // process_siglevel: https://gitlab.archlinux.org/pacman/pacman/-/blob/master/src/pacman/conf.c
     // show_siglevel: https://gitlab.archlinux.org/pacman/pacman/-/blob/master/src/pacman/pacman-conf.c
 
-    let slset = |sl: SigLevel| { original | sl };
-    let slunset = |sl: SigLevel| { original & !sl };
+    let slset = |sl: SigLevel| { (original | sl) & !SigLevel::USE_DEFAULT };
+    let slunset = |sl: SigLevel| { (original & !sl) & !SigLevel::USE_DEFAULT };
 
     let package_trust_all =
         SigLevel::PACKAGE_MARGINAL_OK
@@ -80,67 +85,81 @@ fn main() {
     let root = read_conf([ "RootDir" ]);
     let db_path = read_conf([ "DBPath" ]);
     let all_repos = read_conf([ "--repo-list" ]);
+    eprintln!("RootDir: {root}");
+    eprintln!("DBPath: {db_path}");
+
+    let default_siglevel = default_siglevel();
+    eprintln!("SigLevel::{default_siglevel:?}\n");
 
     let handle = Alpm::new(root, db_path).unwrap();
 
-    let default_siglevel = default_siglevel();
+    // register sync databases from pacman.conf
     for repo in all_repos.split_terminator('\n') {
         let sig_level = repo_siglevel(repo, default_siglevel);
         handle
             .register_syncdb(repo, sig_level)
             .unwrap();
+        // eprintln!("{repo}: SigLevel::{sig_level:?}");
     }
 
-    // iterate through each database
-    for db in handle.syncdbs() {
-        // search each database for packages matching the regex "linux-[a-z]" AND "headers"
-        for pkg in db.search(["linux-[a-z]", "headers"].iter()).unwrap() {
-            println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+    eprint!("Explicit:");
+    for pkg in handle.localdb().pkgs() {
+        if pkg.reason() == PackageReason::Explicit {
+            eprint!(" {}", pkg.name());
         }
     }
+    eprintln!("");
 
-    // iterate through each database
-    for db in handle.syncdbs() {
-        // look for a package named "pacman" in each databse
-        // the database is implemented as a hashmap so this is faster than iterating
-        if let Ok(pkg) = db.pkg("pacman") {
-            println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
-        }
-    }
+    // // iterate through each database
+    // for db in handle.syncdbs() {
+    //     // search each database for packages matching the regex "linux-[a-z]" AND "headers"
+    //     for pkg in db.search(["linux-[a-z]", "headers"].iter()).unwrap() {
+    //         println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+    //     }
+    // }
 
-    // iterate through each database
-    for db in handle.syncdbs() {
-        // iterate through every package in the databse
-        for pkg in db.pkgs() {
-            // print only explititly intalled packages
-            if pkg.reason() == PackageReason::Explicit {
-                println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
-            }
-        }
-    }
+    // // iterate through each database
+    // for db in handle.syncdbs() {
+    //     // look for a package named "pacman" in each databse
+    //     // the database is implemented as a hashmap so this is faster than iterating
+    //     if let Ok(pkg) = db.pkg("pacman") {
+    //         println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+    //     }
+    // }
 
-    // iterate through each database
-    for db in handle.syncdbs() {
-        // look for the base-devel group
-        if let Ok(group) = db.group("base-devel") {
-            // print each package in the group
-            for pkg in group.packages() {
-                println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
-            }
-        }
-    }
+    // // iterate through each database
+    // for db in handle.syncdbs() {
+    //     // iterate through every package in the databse
+    //     for pkg in db.pkgs() {
+    //         // print only explititly intalled packages
+    //         if pkg.reason() == PackageReason::Explicit {
+    //             println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+    //         }
+    //     }
+    // }
 
-    // find a package matching a dep
-    let pkg = handle.syncdbs().find_satisfier("linux>3").unwrap();
-    println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+    // // iterate through each database
+    // for db in handle.syncdbs() {
+    //     // look for the base-devel group
+    //     if let Ok(group) = db.group("base-devel") {
+    //         // print each package in the group
+    //         for pkg in group.packages() {
+    //             println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+    //         }
+    //     }
+    // }
 
-    // load the pacman package from disk instead of from database
-    let pkg = handle
-        .pkg_load(
-            "tests/pacman-5.1.3-1-x86_64.pkg.tar.xz",
-            true,
-            SigLevel::USE_DEFAULT,
-        )
-        .unwrap();
-    println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+    // // find a package matching a dep
+    // let pkg = handle.syncdbs().find_satisfier("linux>3").unwrap();
+    // println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
+
+    // // load the pacman package from disk instead of from database
+    // let pkg = handle
+    //     .pkg_load(
+    //         "tests/pacman-5.1.3-1-x86_64.pkg.tar.xz",
+    //         true,
+    //         SigLevel::USE_DEFAULT,
+    //     )
+    //     .unwrap();
+    // println!("{} {}", pkg.name(), pkg.desc().unwrap_or("None"));
 }
