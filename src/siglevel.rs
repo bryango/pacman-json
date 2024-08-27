f100e172b567f09b54c916be171fed2576dee9bb
Bryan Lai <bryanlais@gmail.com>
2024-08-27 23:03:12 +0800

    Revamp `siglevel.rs`

--- src/siglevel.rs
+++ src/siglevel.rs
@@ -1,18 +1,21 @@
-//! This module defines various utilities to represent the [`SigLevel`] for
+//! A module that defines various utilities to represent the [`SigLevel`] for
 //! pacman repositories, including the [`read_conf`] function to retrieve
 //! configuration from `pacman.conf` via the cli `pacman-conf`.
+//!
+//! Note that similar functionalities are already provided in the `alpm-utils`
+//! crate. This is a minimal re-implementation of components necessary for
+//! `pacjump`.
 
 use alpm::SigLevel;
-use std::{ffi::OsStr, process::Command};
 
 /// Reads pacman.conf via the cli `pacman-conf`. The arguments are directly
 /// passed into [`Command::args`] and the result is parsed into a [`String`].
 pub fn read_conf<I, S>(args: I) -> Result<String, std::io::Error>
 where
     I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
+    S: AsRef<std::ffi::OsStr>,
 {
-    let cmd_out = Command::new("pacman-conf")
+    let cmd_out = std::process::Command::new("pacman-conf")
         .args(args)
         .env("LC_ALL", "C.UTF-8")
         .env("LANGUAGE", "C.UTF-8")
@@ -35,9 +38,9 @@ where
 
 /// Parses and updates a _single_ `SigLevel` line from the cli `pacman-conf`.
 ///
-/// * `siglevel`: [`str`] - the siglevel &str to parse into a [`SigLevel`]
-/// * `prev`: [`SigLevel`] - the previous [`SigLevel`] which would be
-///                          stacked onto by the newly parsed `siglevel`
+/// * `default`: [`SigLevel`] - the default [`SigLevel`] which would be
+///                             stacked onto by the newly parsed `siglevel`
+/// * `siglevel`: `&str` - the siglevel string to parse into a [`SigLevel`]
 ///
 /// The `SigLevel` return of `pacman-conf` is a fine-grained subset of those
 /// allowed in `pacman.conf`. For example, `SigLevel = Required` in
@@ -60,11 +63,11 @@ where
 /// #
 /// // when an empty string is passed, the siglevel is unmodified;
 /// // whitespace is ignored:
-/// let siglevel = process_siglevel("\n\t \n", SigLevel::PACKAGE_OPTIONAL);
+/// let siglevel = process_siglevel(SigLevel::PACKAGE_OPTIONAL, "\n\t \n");
 /// assert_eq!(siglevel, SigLevel::PACKAGE_OPTIONAL);
 ///
 /// // demands that packages require a signature:
-/// let siglevel = process_siglevel("PackageRequired", SigLevel::USE_DEFAULT);
+/// let siglevel = process_siglevel(SigLevel::USE_DEFAULT, "PackageRequired");
 /// assert_eq!(siglevel, SigLevel::PACKAGE);
 /// ```
 ///
@@ -73,15 +76,15 @@ where
 /// ```should_panic
 /// # use alpm::SigLevel;
 /// # use pacjump::siglevel::process_siglevel;
-/// process_siglevel("NonExistentSigLevel", SigLevel::USE_DEFAULT); // panic!
+/// process_siglevel(SigLevel::USE_DEFAULT, "NonExistentSigLevel"); // panic!
 ///
 /// // only fine-grained SigLevels are allowed:
-/// process_siglevel("Required", SigLevel::USE_DEFAULT); // panic!
+/// process_siglevel(SigLevel::USE_DEFAULT, "Required"); // panic!
 /// ```
 ///
-pub fn process_siglevel(siglevel: &str, prev: SigLevel) -> SigLevel {
-    let slset = |sl: SigLevel| (prev | sl) & !SigLevel::USE_DEFAULT;
-    let slunset = |sl: SigLevel| (prev & !sl) & !SigLevel::USE_DEFAULT;
+pub fn process_siglevel(default: SigLevel, siglevel: &str) -> SigLevel {
+    let slset = |sl: SigLevel| (default | sl) & !SigLevel::USE_DEFAULT;
+    let slunset = |sl: SigLevel| (default & !sl) & !SigLevel::USE_DEFAULT;
 
     let package_trust_all = SigLevel::PACKAGE_MARGINAL_OK | SigLevel::PACKAGE_UNKNOWN_OK;
     let database_trust_all = SigLevel::DATABASE_MARGINAL_OK | SigLevel::DATABASE_UNKNOWN_OK;
@@ -97,7 +100,7 @@ pub fn process_siglevel(siglevel: &str, prev: SigLevel) -> SigLevel {
         "DatabaseRequired" => slset(SigLevel::DATABASE) & !SigLevel::DATABASE_OPTIONAL,
         "DatabaseTrustedOnly" => slunset(database_trust_all),
         "DatabaseTrustAll" => slset(database_trust_all),
-        "" => prev,
+        "" => default,
         x => panic!("failed to parse the signature level: {}", x),
     }
 }
@@ -118,32 +121,32 @@ pub fn process_siglevel(siglevel: &str, prev: SigLevel) -> SigLevel {
 /// ].join("\n");
 ///
 /// use SigLevel as Sig;
-/// let siglevel = fold_siglevels(siglevel, Sig::USE_DEFAULT);
+/// let siglevel = fold_siglevels(Sig::USE_DEFAULT, siglevel);
 /// assert_eq!(siglevel, Sig::PACKAGE | Sig::DATABASE | Sig::DATABASE_OPTIONAL);
 ///
 /// // empty lines are ignored:
-/// let siglevel = fold_siglevels("\n\n\n".into(), Sig::DATABASE_OPTIONAL);
+/// let siglevel = fold_siglevels(Sig::DATABASE_OPTIONAL, "\n\n\n".into());
 /// assert_eq!(siglevel, Sig::DATABASE_OPTIONAL);
 ///
-/// let siglevel = fold_siglevels("".into(), Sig::DATABASE_OPTIONAL);
+/// let siglevel = fold_siglevels(Sig::DATABASE_OPTIONAL, "".into());
 /// assert_eq!(siglevel, Sig::DATABASE_OPTIONAL);
 /// ```
 ///
-pub fn fold_siglevels(siglevels: String, default: SigLevel) -> SigLevel {
+pub fn fold_siglevels(default: SigLevel, siglevels: String) -> SigLevel {
     siglevels
         .split_terminator('\n')
-        .fold(default, |prev, new| process_siglevel(new, prev))
+        .fold(default, process_siglevel)
 }
 
 /// Finds the default SigLevel from `pacman.conf`; if it fails, fall back to
 /// the default [`SigLevel::USE_DEFAULT`].
 pub fn default_siglevel() -> SigLevel {
     let siglevels = read_conf(["SigLevel"]).unwrap_or("".into());
-    fold_siglevels(siglevels, SigLevel::USE_DEFAULT)
+    fold_siglevels(SigLevel::USE_DEFAULT, siglevels)
 }
 
 /// Finds the SigLevel of a repo; if it fails, fall back to the `default`.
 pub fn repo_siglevel(repo: &str, default: SigLevel) -> SigLevel {
     let siglevels = read_conf(["--repo=", &repo, "SigLevel"]).unwrap_or("".into());
-    fold_siglevels(siglevels, default)
+    fold_siglevels(default, siglevels)
 }
